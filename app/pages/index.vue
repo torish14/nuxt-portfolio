0473787dcd94174bcb3324f608fdc029adcbb0ca
toshihirokato <nanamin.104@icloud.com>
2020-02-01 04:43:47 +0900

    バブルチャートにこめ

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,18 +3,23 @@
     <div class="titie" style="font-size: 60px;">
       Strengths Finder
     </div>
-    <div class="bubble" style="pointer-events: none;">
+    <div class="bubble1" style="pointer-events: none;">
       <Bubble1 />
+    </div>
+    <div class="bubble" style="pointer-events: none;">
+      <Bubble2 />
     </div>
   </section>
 </template>
 
 <script>
 import Bubble1 from '~/components/Bubble1'
+import Bubble2 from '~/components/Bubble2'
 
 export default {
   components: {
-    Bubble1
+    Bubble1,
+    Bubble2
   },
   data () {
     return {
@@ -25,7 +30,7 @@ export default {
 </script>
 
 <style>
-.bubble {
+.bubble1 {
   padding-top: 50px;
 }
 </style>
