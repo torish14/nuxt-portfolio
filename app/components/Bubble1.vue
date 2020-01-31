75bad34a8d90947becb5783e970bea7b5396bccf
toshihirokato <nanamin.104@icloud.com>
2020-02-01 02:06:12 +0900

    バブルチャートの表示

---
+++ app/components/Bubble1.vue
@@ -0,0 +1,84 @@
+<script>
+import { Bubble } from 'vue-chartjs'
+
+export default {
+  extends: Bubble,
+  data () {
+    return {
+      data: {
+        datasets: [
+          {
+            backgroundColor: 'rgba(255, 204, 0, 0.4)', // 塗り潰しの色
+            borderColor: 'rgba(255, 204, 0, 1)', // 線の色
+            pointBackgroundColor: 'rgba(255, 204, 0, 1)', // 点の塗り潰しの色
+            pointHitRadius: 20,
+            pointStyle: 'circle',
+            pointRadius: 2,
+            data: [
+              {
+                x: 100,
+                y: 0,
+                r: 10
+              },
+              {
+                x: 60,
+                y: 30,
+                r: 20
+              },
+              {
+                x: 40,
+                y: 60,
+                r: 25
+              },
+              {
+                x: 80,
+                y: 80,
+                r: 50
+              },
+              {
+                x: 20,
+                y: 30,
+                r: 25
+              },
+              {
+                x: 0,
+                y: 100,
+                r: 5
+              }
+            ]
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: false
+            },
+            gridLines: {
+              display: false
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              beginAtZero: false
+            },
+            gridLines: {
+              display: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        height: 200
+      }
+    }
+  },
+  mounted () {
+    this.renderChart(this.data, this.options)
+  }
+}
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>Strengths Finder</div>
+  <section class="container">
+    <div class="titie">
+      Strengths Finder
+    </div>
+    <div class="bubble">
+      <Bubble1 />
+    </div>
+  </section>
 </template>
 
 <script>
+import Bubble1 from '~/components/Bubble1'
+
 export default {
+  components: {
+    Bubble1
+  },
   data () {
     return {
 
