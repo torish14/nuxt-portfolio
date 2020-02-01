ccba514092242dad7b1389fbd2fc4d233fcdc533
toshihirokato <nanamin.104@icloud.com>
2020-02-01 18:12:52 +0900

    カード表示

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,23 @@
 <template>
   <section class="container">
-    <div class="titie" style="font-size: 70px;">
-      <div class="front" style="font-weight: 100;">
+    <div class="titie" style="font-size: 70px; font-family: Cambay;">
+      <div class="front" style="font-weight: 400; text-shadow: 1px 1px 3px #808080">
         Strengths
       </div>
-      <div class="back" style="font-weight: 100;">
+      <div class="back" style="font-weight: 300; text-shadow: 1px 1px 2px #808080">
         Finder
       </div>
     </div>
     <div class="bubble1" style="pointer-events: none;">
       <Bubble1 />
+    </div>
+    <div class="heading" style="font-size: 40px; font-weight: 100; padding-bottom: 20px;">
+      Formation
+    </div>
+    <div class="card">
+      <Card1 />
+      <Card2 />
+      <Card3 />
     </div>
     <div class="bubble2" style="pointer-events: none;">
       <Bubble2 />
@@ -28,13 +36,19 @@ import Bubble1 from '~/components/Bubble1'
 import Bubble2 from '~/components/Bubble2'
 import Bubble3 from '~/components/Bubble3'
 import Bubble4 from '~/components/Bubble4'
+import Card1 from '~/components/Card1'
+import Card2 from '~/components/Card2'
+import Card3 from '~/components/Card3'
 
 export default {
   components: {
     Bubble1,
     Bubble2,
     Bubble3,
-    Bubble4
+    Bubble4,
+    Card1,
+    Card2,
+    Card3
   },
   data () {
     return {
@@ -53,13 +67,22 @@ export default {
   padding-right: 50px;
 }
 
+.header {
+  position: relative;
+}
+
+.card {
+  display: flex;
+}
+
 .bubble1 {
-  padding-top: 50px;
+  position: relative;
+  top:-80px;
 }
 
 .bubble2 {
   padding-left: 50px;
-  padding-top: 50px;
+  padding-top: -50px;
 }
 
 .bubble3 {
