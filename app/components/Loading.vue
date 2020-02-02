208e0f7162f4887df55e2357f17aa0d43f3dcf0b
toshihirokato <nanamin.104@icloud.com>
2020-02-03 06:10:16 +0900

    Loadingコンポーネントの作成

---
+++ app/components/Loading.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center">
+    <v-progress-circular
+      :size="70"
+      :width="2"
+      color="purple"
+      indeterminate
+    />
+  </div>
+</template>
+
+<style scoped>
+.v-progress-circular {
+  margin: 1rem;
+  top: 250px;
+  right: 120px;
+}
+</style>
