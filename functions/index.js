d0ee941a7abcd3f5d98aa94a2b4d7640f472b6da
toshihirokato <nanamin.104@icloud.com>
2020-03-29 18:17:30 +0900

    cors の削除

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+const firebase = require('firebase')
 // const functions = require('firebase-functions')
 const functions = firebase.app().functions('asia-northeast1')
 // const nuxtServer = require('./nuxt-server')
@@ -5,7 +6,7 @@ const nodemailer = require('nodemailer')
 const gmailEmail = functions.config().gmail.email
 const gmailPassword = functions.config().gmail.password
 const adminEmail = functions.config().admin.email
-const cors = require('cors')({ origin: true })
+// const cors = require('cors')({ origin: true })
 
 // 送信に使用するメールサーバーの設定
 const mailTransport = nodemailer.createTransport({
@@ -19,19 +20,16 @@ const mailTransport = nodemailer.createTransport({
 // 管理者用メールアドレス
 const adminContents = (data) => {
   return `I received a contact from the website with the following contents.
-
 name:
 ${data.name}
-
 email:
 ${data.email}
-
 contents:
 ${data.contents}
 `
 }
 
-exports.sendMailNew = functions.region('asia-northeast1').https.onCall((data, context) => {
+exports.sendMailNew = functions.https.onCall(async (data, context) => {
   // メール設定
   const adminMail = {
     from: gmailEmail,
@@ -41,12 +39,16 @@ exports.sendMailNew = functions.region('asia-northeast1').https.onCall((data, co
   }
 
   // 管理者へのメール送信
-  cors(mailTransport.sendMailNew(adminMail, (err, info) => {
-    if (err) {
-      return console.error(`admin send failed. ${err}`)
-    }
-    return console.log('admin and user success')
-  }))
+  // cors(mailTransport.sendMailNew(adminMail, (err, info) => {
+  //   if (err) {
+  //     return console.error(`admin send failed. ${err}`)
+  //   }
+  //   return console.log('admin and user success')
+  // }))
+  try {
+    await mailTransport.sendMailNew(adminMail)
+  } catch (error) {
+    console.error(`admin send failed. ${e}`)
+    throw new functions.https.HttpsError('internal', 'send failed')
+  }
 })
-
-// exports.nuxtServer = functions.https.onRequest(nuxtServer)
